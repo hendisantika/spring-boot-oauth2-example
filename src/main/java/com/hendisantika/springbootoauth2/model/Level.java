f529ded4fea10b5b4fe4ce67e75e1983d0212925
Hendi Santika <hendisantika@gmail.com>
2017-08-27 05:25:27 +0700

    #3 - Added Enum Level

---
+++ src/main/java/com/hendisantika/springbootoauth2/model/Level.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springbootoauth2.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 8/27/17
+ * Time: 5:24 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Level {
+    GOOD, AWESOME, GODLIKE
+}
