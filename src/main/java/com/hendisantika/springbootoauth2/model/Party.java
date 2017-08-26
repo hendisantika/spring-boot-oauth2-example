5611b40092977f68b6c88c221d94b717f5c72073
Hendi Santika <hendisantika@gmail.com>
2017-08-27 05:27:57 +0700

    #5 - Added Party Model

--- src/main/java/com/hendisantika/springbootoauth2/model/Party.java
+++ src/main/java/com/hendisantika/springbootoauth2/model/Party.java
@@ -1,5 +1,10 @@
 package com.hendisantika.springbootoauth2.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+
 /**
  * Created by IntelliJ IDEA.
  * User: hendisantika
@@ -9,5 +14,56 @@ package com.hendisantika.springbootoauth2.model;
  * Time: 5:25 AM
  * To change this template use File | Settings | File Templates.
  */
+
+@Entity
+@Table(name = "parties")
 public class Party {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "party_id")
+    private long id;
+
+    private String location;
+
+    @Column(name = "party_date")
+    @JsonFormat(pattern = "YYYY-MM-dd")
+    private Date date;
+
+    @ManyToMany
+    @JoinTable(name = "people_parties",
+            joinColumns = @JoinColumn(name = "party_id", referencedColumnName = "party_id"),
+            inverseJoinColumns = @JoinColumn(name = "person_id", referencedColumnName = "person_id"))
+    private Set<Person> people = new HashSet<Person>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Set<Person> getPeople() {
+        return people;
+    }
+
+    public void setPeople(Set<Person> people) {
+        this.people = people;
+    }
 }
