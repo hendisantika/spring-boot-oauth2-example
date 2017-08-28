9becff5f1501fb9c18903f754e13549d6c4f68e3
Hendi Santika <hendisantika@gmail.com>
2017-08-28 11:17:05 +0700

    #11 - Added Buddy Service

---
+++ src/main/java/com/hendisantika/springbootoauth2/service/BuddyService.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootoauth2.service;
+
+import com.hendisantika.springbootoauth2.model.Person;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 8/28/17
+ * Time: 11:16 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface BuddyService {
+    public Person findBuddy(Person person);
+}
