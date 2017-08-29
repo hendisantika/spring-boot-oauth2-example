755a2951defff36379c97e6e791a97e1cf41419d
Hendi Santika <hendisantika@gmail.com>
2017-08-30 06:00:31 +0700

    #19 - Added Repository Test

---
+++ src/test/java/com/hendisantika/springbootoauth2/repository/PartyRepositoryTest.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootoauth2.repository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 8/30/17
+ * Time: 6:00 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PartyRepositoryTest {
+}
