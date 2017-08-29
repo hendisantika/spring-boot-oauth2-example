7d93fe13e9999f8a1d42fd8828bc9979bd6c388d
Hendi Santika <hendisantika@gmail.com>
2017-08-30 06:01:45 +0700

    #20 - Modified Party Repository Test

--- src/test/java/com/hendisantika/springbootoauth2/repository/PartyRepositoryTest.java
+++ src/test/java/com/hendisantika/springbootoauth2/repository/PartyRepositoryTest.java
@@ -1,5 +1,14 @@
 package com.hendisantika.springbootoauth2.repository;
 
+import com.hendisantika.springbootoauth2.model.Party;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by IntelliJ IDEA.
  * User: hendisantika
@@ -9,5 +18,20 @@ package com.hendisantika.springbootoauth2.repository;
  * Time: 6:00 AM
  * To change this template use File | Settings | File Templates.
  */
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
 public class PartyRepositoryTest {
+    @Autowired
+    private PartyRepository repository;
+
+    @Test
+    public void repositorySavesParty() {
+        Party party = new Party();
+        party.setLocation("Bandung");
+
+        Party result = repository.save(party);
+
+        assertEquals(result.getLocation(), "Bandung");
+    }
 }
